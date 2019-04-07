da1049e95f2bd150763c41d0322a1b93e3cd9519
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2019-04-07 11:08:58 +0200

    Added support for multiple versions, es6

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var editor = ace.edit("editor");
+const editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
 editor.$blockScrolling = Infinity;
-editor.setValue((!localStorage.code ? "" : localStorage.code));
-var Storage = {
-  save: function() {
+const Storage = {
+  save() {
     localStorage.code = editor.getValue();
   },
-  load: function() {
-    editor.setValue((!localStorage.code ? "" : localStorage.code));
+  load() {
+    editor.setValue(localStorage.code || '');
   },
-  reset: function() {
+  reset() {
     if(confirm('Are you sure?')) {
       editor.setValue(null);
       localStorage.code = null;
     }
   }
 };
-var Engine = {
+Storage.load();
+const Engine = {
   started: false,
-  start: function() {
-    if(Engine.started) if(!confirm('The client has already started, clicking on YES might result in double events')) return;
+  start() {
+    if(Engine.started) if(!confirm('The client has already started, clicking on YES might result in double instances')) return;
     try {
       eval(editor.getValue());
       Engine.started = true;
@@ -29,7 +29,22 @@ var Engine = {
       Engine.started = false;
     }
   },
-  stop: function() {
+  stop() {
     location.reload();
+  },
+  newVersion(version) {
+    if(Engine.started) return alert('Client must not be running');
+    delete window.Discord;
+    document.getElementsByTagName('select')[0].disabled = true;
+    document.getElementsByTagName('script')[0].remove();
+    const script = document.createElement('script');
+    script.src = 'discord.' + version + '.min.js';
+    script.addEventListener('load', () => {
+      document.getElementsByTagName('select')[0].disabled = false;
+    });
+    document.head.appendChild(script);
+    localStorage.version = version;
   }
 };
+if(localStorage.version !== document.getElementsByTagName('select')[0].value && localStorage.version !== undefined)
+  Engine.newVersion(localStorage.version);
